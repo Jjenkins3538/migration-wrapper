4fbc43c61584b525e88ac3fdd35d4c4fb208b6d4
DeonteHorton <deontedeshaunhorton@gmail.com>
2023-09-28 18:14:51 -0500

    here ya go

--- app/Services/MigrationRulesService.php
+++ app/Services/MigrationRulesService.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Schema\Grammars\MySqlGrammar;
 
 class MigrationRulesService
 {
-
     private $dbDriver;
 
     /**
@@ -62,3 +61,296 @@ class MigrationRulesService
         }
     }
 }
+
+
+//TODO
+// return [
+//     'string' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'text' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'mediumText' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'longText' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'integer' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'bigInteger' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'unsignedBigInteger' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'float' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'double' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'decimal' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'boolean' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'date' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'dateTime' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'dateTimeTz' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'time' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'timeTz' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'year' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'json' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'jsonb' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'uuid' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'ipAddress' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'macAddress' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'geometry' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'point' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'lineString' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'polygon' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'geometryCollection' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'multiPoint' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'multiLineString' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'multiPolygon' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'set' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+//     'enum' => [
+//         'type' => '',
+//         'nullable' => false,
+//         'default' => null,
+//         'table_name' => '',
+//         'modifiers' => [
+
+//         ],
+//     ] ,
+// ];
