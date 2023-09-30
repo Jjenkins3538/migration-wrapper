acdaf3a7f93fd81eb44e93842d47f11063e8ec53
Jameson Jenkins <jameson239@gmail.com>
2023-09-30 13:44:21 -0500

    finished creating the create migration class logic with columns :)

--- app/Console/Commands/MigrationCreator.php
+++ app/Console/Commands/MigrationCreator.php
@@ -2,13 +2,14 @@
 
 namespace App\Console\Commands;
 
+use App\Services\GenerateMigrationClassService;
 use Illuminate\Support\Str;
 use InvalidArgumentException;
 use Illuminate\Console\Command;
 use function Laravel\Prompts\text;
+use function Laravel\Prompts\confirm;
 use Illuminate\Filesystem\Filesystem;
 use App\Services\MigrationRulesService;
-
 
 class MigrationCreator extends Command
 {
@@ -38,11 +39,14 @@ class MigrationCreator extends Command
      */
     public function handle()
     {
+        // NOTE: MOVED EVERYTHING FROM STUB TO SERVICE
+
         // TODO:
         // first check which DB driver is being used (mysql, pgsql, sqlite, sqlsrv) it effects which rules are allowed
         // fill rules service with known migration rules
         // prompt user for migration rules
-        // logic for which stub to load, probably just str::contains tbh
+        // add update class to GenerateMigrationClassService
+
 
 
         $this->files = new Filesystem();
@@ -74,6 +78,16 @@ class MigrationCreator extends Command
 
         $filePath = database_path('migrations/' . date('Y_m_d_His') . '_' . $tableName . '.php');
         $this->populateStubFile($tableName, $filePath, $cleanedColumns);
+
+        $confirm = confirm(
+            label: 'Would you like to run the migration now?',
+            default: false
+        );
+
+        if ($confirm) {
+            shell_exec('php artisan migrate');
+        }
+
     }
 
     /**
@@ -96,36 +110,29 @@ class MigrationCreator extends Command
         }
     }
 
-    private function getStub()
-    {
-        return $this->files->get(storage_path('app/public/stubs/migration.create.stub'));
-    }
-
     private function populateStubFile(string $tableName, string $filePath, string $columns): void
     {
-        $stub = $this->getStub();
+        $migrationClassString = new GenerateMigrationClassService();
+        $migrationClassString = $migrationClassString->__invoke();
 
+        // Create the migration file
         $tableVar = Str::between($tableName, 'create_', '_table');
-        $stub = str_replace('{{ table }}', $tableVar, $stub);
-        $stub = str_replace('{{ columns }}', $columns, $stub);
-
-        $this->files->put($filePath, $stub);
+        $migrationClassString = str_replace('{{ table }}', $tableVar, $migrationClassString);
+        $migrationClassString = str_replace('{{ columns }}', $columns, $migrationClassString);
+        $this->files->put($filePath, $migrationClassString);
     }
 
     private function cleanColumns(string $columnString): string
     {
         $columns = explode(', ', $columnString);
-        $cleanedColumns = [];
+        $cleanedColumns = '';
         $baseString = "\$table->string('{column}');";
 
         foreach ($columns as $column) {
             $columnName = str_replace('{column}', Str::snake($column), $baseString);
 
-            $cleanedColumns[] = $columnName;
+            $cleanedColumns .= $columnName . PHP_EOL . '                    ';
         }
-
-        $cleanedColumns = implode(PHP_EOL . '        ', $cleanedColumns);
-        logger($cleanedColumns);
 
         return $cleanedColumns;
     }
